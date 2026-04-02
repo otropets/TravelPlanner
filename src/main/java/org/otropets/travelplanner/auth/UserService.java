e42e607bd5ee404292f7adba9f388d0091234b2b
Oleksandr Tropets <245129@edu.p.lodz.pl>
2026-04-02 21:46:13 +0200

    Added Servie and Controller, added SecurityFilterChain to config

--- src/main/java/org/otropets/travelplanner/auth/UserService.java
+++ src/main/java/org/otropets/travelplanner/auth/UserService.java
@@ -1,4 +1,49 @@
 package org.otropets.travelplanner.auth;
 
+import org.otropets.travelplanner.auth.dto.AuthResponse;
+import org.otropets.travelplanner.auth.dto.LoginRequest;
+import org.otropets.travelplanner.auth.dto.RegisterRequest;
+import org.otropets.travelplanner.security.JwtService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, JwtService jwtService, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.jwtService = jwtService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public AuthResponse register(RegisterRequest registerRequest)
+    {
+        if(userRepository.existsByEmail(registerRequest.getEmail()) || userRepository.existsByUsername(registerRequest.getUsername()))
+        {
+            throw new RuntimeException("Email or username already taken");
+        }
+
+        String hashedPassword = passwordEncoder.encode(registerRequest.getPassword());
+        User user = User.builder().username(registerRequest.getUsername()).email(registerRequest.getEmail()).password(hashedPassword).firstName(registerRequest.getFirstName()).lastName(registerRequest.getLastName()).build();
+        userRepository.save(user);
+        String token = jwtService.generateToken(registerRequest.getEmail());
+        return new AuthResponse(user.getUsername(), user.getEmail(), token);
+    }
+
+    public AuthResponse login(LoginRequest loginRequest)
+    {
+      User user = userRepository.findByEmail(loginRequest.getEmail()).orElseThrow(() ->new RuntimeException("User not found"));
+      if(!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword()))
+      {
+          throw new RuntimeException("Invalid password");
+      }
+      String token = jwtService.generateToken(user.getEmail());
+      return new AuthResponse(user.getUsername(), user.getEmail(), token);
+    }
+
+
 }
