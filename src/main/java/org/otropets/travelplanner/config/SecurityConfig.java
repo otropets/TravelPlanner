4bd6e038bf96df0abd4d37b3a5c5c51bc182111b
Oleksandr Tropets <245129@edu.p.lodz.pl>
2026-04-03 20:21:42 +0200

    User module done, tests added, coverage 100%

--- src/main/java/org/otropets/travelplanner/config/SecurityConfig.java
+++ src/main/java/org/otropets/travelplanner/config/SecurityConfig.java
@@ -1,30 +1,26 @@
 package org.otropets.travelplanner.config;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.otropets.travelplanner.security.JwtService;
+import org.otropets.travelplanner.auth.UserPrincipal;
+import org.otropets.travelplanner.security.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.apache.catalina.webresources.TomcatURLStreamHandlerFactory.disable;
 
 @Configuration
 public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
      return new BCryptPasswordEncoder();
@@ -32,52 +28,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
-                .csrf(csrf -> disable())
+                .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("api/auth/**").permitAll()
-                .anyRequest().authenticated());
+                        auth.requestMatchers("/api/auth/**").permitAll()
+                .anyRequest().authenticated()).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
-    }
-    @Component
-    public class JwtAuthFilter extends OncePerRequestFilter {
-
-        private final JwtService jwtService;
-        private final UserDetailsService userDetailsService;
-
-        public JwtAuthFilter(JwtService jwtService, UserDetailsService userDetailsService) {
-            this.jwtService = jwtService;
-            this.userDetailsService = userDetailsService;
-        }
-
-        @Override
-        protected void doFilterInternal(HttpServletRequest request,
-                                        HttpServletResponse response,
-                                        FilterChain filterChain) throws ServletException, IOException {
-
-            String authHeader = request.getHeader("Authorization");
-
-            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-                filterChain.doFilter(request, response);
-                return;
-            }
-
-            String token = authHeader.substring(7);
-            String email = jwtService.extractUsername(token);
-
-            if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                UserDetails userDetails = userDetailsService.loadUserByUsername(email);
-
-                if (jwtService.isTokenValid(token, email)) {
-                    UsernamePasswordAuthenticationToken authToken =
-                            new UsernamePasswordAuthenticationToken(
-                                    userDetails, null, userDetails.getAuthorities());
-
-                    SecurityContextHolder.getContext().setAuthentication(authToken);
-                }
-            }
-            filterChain.doFilter(request, response);
-        }
     }
 }
---
+++ src/test/java/org/otropets/travelplanner/UserServiceTest.java
@@ -0,0 +1,107 @@
+package org.otropets.travelplanner;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.otropets.travelplanner.auth.User;
+import org.otropets.travelplanner.auth.UserRepository;
+import org.otropets.travelplanner.auth.UserService;
+import org.otropets.travelplanner.auth.dto.AuthResponse;
+import org.otropets.travelplanner.auth.dto.LoginRequest;
+import org.otropets.travelplanner.auth.dto.RegisterRequest;
+import org.otropets.travelplanner.security.JwtService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @Mock
+    UserRepository userRepository;
+    @InjectMocks
+    UserService userService;
+    @Mock
+    JwtService jwtService;
+    @Mock
+    PasswordEncoder passwordEncoder;
+
+    @Test
+    void successfulRegister(){
+        when(userRepository.existsByEmail(any())).thenReturn(false);
+        when(userRepository.existsByUsername(any())).thenReturn(false);
+        when(passwordEncoder.encode(any())).thenReturn("hashed_password");
+        when(jwtService.generateToken(any())).thenReturn("token");
+
+        RegisterRequest registerRequest = new RegisterRequest(
+                "john", "password", "john@gmail.com", "John", "Abc");
+
+        AuthResponse response = userService.register(registerRequest);
+
+        assertNotNull(response);
+        assertEquals("john", response.getUsername());
+        assertEquals("john@gmail.com", response.getEmail());
+        assertEquals("token", response.getToken());
+    }
+    @Test
+    void unsuccessfulRegisterUsernameAlreadyTaken(){
+        when(userRepository.existsByUsername(any())).thenReturn(true);
+
+        RegisterRequest registerRequest = new RegisterRequest(
+                "john", "password", "john@gmail.com", "John", "Abc");
+
+        assertThrows(RuntimeException.class, () -> userService.register(registerRequest));
+    }
+    @Test
+    void unsuccessfulRegisterEmailAlreadyTaken(){
+        when(userRepository.existsByEmail(any())).thenReturn(true);
+
+        RegisterRequest registerRequest = new RegisterRequest(
+                "john", "password", "john@gmail.com", "John", "Abc");
+
+        assertThrows(RuntimeException.class, () -> userService.register(registerRequest));
+    }
+    @Test
+    void successfulLogin()
+    {
+        User mockUser = User.builder().username("john").firstName("John").lastName("Abc").password("hashed_password").email("john@gmail.com").build();
+        when(userRepository.findByEmail(any())).thenReturn(Optional.of(mockUser));
+        when(passwordEncoder.matches(any(), any())).thenReturn(true);
+        when(jwtService.generateToken(any())).thenReturn("token");
+
+        LoginRequest loginRequest = new LoginRequest("john@gmail.com", "password");
+        AuthResponse authResponse = userService.login(loginRequest);
+
+        assertNotNull(authResponse);
+        assertEquals("john@gmail.com", authResponse.getEmail());
+        assertEquals("john",authResponse.getUsername());
+        assertEquals("token", authResponse.getToken());
+    }
+    @Test
+    void unsuccessfulLoginUserNotFound()
+    {
+        when(userRepository.findByEmail(any())).thenReturn(Optional.empty());
+        LoginRequest loginRequest = new LoginRequest("john@gmail.com", "passsword");
+
+        assertThrows(RuntimeException.class, () -> userService.login(loginRequest));
+    }
+    @Test
+    void unsuccessfulLoginWrongPassword()
+    {
+        User mockUser = User.builder().username("john").firstName("John").lastName("Abc").password("hashed_password").email("john@gmail.com").build();
+        when(userRepository.findByEmail(any())).thenReturn(Optional.of(mockUser));
+        when(passwordEncoder.matches(any(), any())).thenReturn(false);
+
+        LoginRequest loginRequest = new LoginRequest("john@email.com", "password");
+
+        assertThrows(RuntimeException.class, ()-> userService.login(loginRequest));
+
+    }
+
+}
