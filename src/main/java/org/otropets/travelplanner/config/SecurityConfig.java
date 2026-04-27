26b56ec6edf708ed9530f243c4699afb166e082b
Oleksandr Tropets <245129@edu.p.lodz.pl>
2026-04-27 15:30:25 +0200

    Frontend URL added

--- src/main/java/org/otropets/travelplanner/config/SecurityConfig.java
+++ src/main/java/org/otropets/travelplanner/config/SecurityConfig.java
@@ -47,8 +47,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "http://localhost:5174",
-                "http://localhost:5175",
-                "http://localhost:5176"
+                "https://your-frontend-url.vercel.app"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
